cbe480522f3018274d8d62d20d9d46df0aab5ea7
MatiasEM12 <matiasemimellado@gmail.com>
2026-06-14 00:33:53 -0300

    base ejercicio 3

---
+++ src/main/java/ejercicio_3/Golondrina.java
@@ -0,0 +1,22 @@
+package ejercicio_3;
+
+
+public class Golondrina {
+    private int energia; //joules
+
+    public Golondrina() {
+        this.energia = 45;
+    }
+
+    public void comer() {
+        // implementar ...
+    }
+
+    public void volar() {
+        // implementar ...
+    }
+
+    public void realizarDeseo() {
+        //implementar ...
+    }
+}
